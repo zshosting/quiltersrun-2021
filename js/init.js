d66cc33ac918cb30f0b790dba5f3d10d8460b340
poontang <poontang@users.noreply.github.com>
2021-05-27 20:12:47 -0700

    dx

--- js/init.js
+++ js/init.js
@@ -1,7 +1,3 @@
-(function($){
-  $(function(){
-
-    $('.sidenav').sidenav();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(document).ready(function(){
+    $('.materialboxed').materialbox();
+  });
